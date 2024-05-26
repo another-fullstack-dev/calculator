39fc26e61cb930bb9ba0394b772cd2298d6bc30e
draemato <97753719+draemato@users.noreply.github.com>
2024-05-26 21:26:48 +0700

    Can perform calculations

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function divide(num1, num2){
     if (num1 == 0 || num2 == 0) return alert("Cant divide by zero.")
     return num1 / num2;
 }
+
+let firstNumber;
+let secondNumber;
+let operation;
+
+function operate(firstNumber, operation, secondNumber){
+    if (firstNumber === undefined || operation === undefined){
+        return alert("Invalid input");
+    }
+
+    let result;
+
+    switch (operation){
+        case '+':
+            result = add(firstNumber, secondNumber);
+            break;
+
+        case '-':
+            result = subtract(firstNumber, secondNumber);
+            break;
+        
+        case '*':
+            result = multiply(firstNumber, secondNumber);
+            break;
+
+        case '/':
+            result = divide(firstNumber, secondNumber);
+            break;
+        
+        default:
+            alert("Something went wrong.");
+    }
+
+    return result;
+}
